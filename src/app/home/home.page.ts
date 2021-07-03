60411f9116fedcb50c3a83c68f2922e119f4c892
Vetrivel <vetrivelkalidass10700@gmail.com>
2021-07-03 21:34:20 +0530

    basic UI components added for testing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  createExpense(expenseForm: NgForm) {
+    console.log(expenseForm.value);
+  }
 
 }
