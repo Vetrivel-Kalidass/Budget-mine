23d4072773d3518c28c15b1ddd7e7b2921953b07
vetrivel-light <vetrivelkalidass10700@gmail.com>
2021-07-04 07:22:01 +0530

    list, form inputs, grids

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface expense {
+  title: string
+  reason: string
+  amount: number
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+  expenseList: expense[] = [];
+  totalExpAmt: number;
+
+  ngOnInit() {
+    this.totalExpAmt = this.expenseList.reduce((acc, val) => acc + val.amount, 0);
+  }
+
   createExpense(expenseForm: NgForm) {
-    console.log(expenseForm.value);
+    if (expenseForm.invalid) return;
+    this.expenseList.push(expenseForm.value);
+    this.totalExpAmt = this.expenseList.reduce((acc, val) => acc + val.amount, 0);
   }
 
 }
